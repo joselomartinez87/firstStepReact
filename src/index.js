d4ea2593c1476cccd8b90cb30beb0370c004d696
jmartinezt <jmartinezt@credivalores.com>
2020-11-24 12:37:49 -0500

    add redux-thunk

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
+// import { thunk } from 'redux-thunk';
+import thunk, { ThunkMiddleware } from "redux-thunk";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root')
   // <React.StrictMode>
