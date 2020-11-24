73ce06282021e984f3529340d7acaaaf36e19e3b
jmartinezt <jmartinezt@credivalores.com>
2020-11-24 09:50:45 -0500

    update projectReducer

--- src/store/reducers/projectreducer.js
+++ src/store/reducers/projectreducer.js
@@ -1,8 +1,8 @@
 const initState = {
     projects: [
         {id: '1', title: 'help me find peach', content:'bla bla'},
-        {id: '2', title: 'help me find peach', content:'bla bla2'},
-        {id: '3', title: 'help me find peach', content:'bla bla3'}
+        {id: '2', title: 'collect all the stars', content:'bla bla2'},
+        {id: '3', title: 'egg hubt with yoshi', content:'bla bla3'}
     ]
 }
 
